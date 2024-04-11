014963b99ebc1fcd3c3796b677983941454bef7e
letiiciamelo89 <leticinhamelo576@gmail.com>
2024-04-11 11:39:22 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.name"letiiciamelo89"
+git config --global user.email"leticinhamelo576@gmail.com"
+
+cd C:\Users\LETICIADEMELOFIGUEIR\
+
+git clone https://github.com/letiiciamelo89/caesar-cipher.git
+cd caesar-cipher
+
+code .
